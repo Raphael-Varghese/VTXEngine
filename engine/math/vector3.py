51acba605f5a992f30cc4a1076cfce2858b9c9a7
Code_Boss <s20170468@students.duvalschools.org>
2025-10-07 10:46:01 -0400

    Create vector3.py

---
+++ engine/math/vector3.py
@@ -0,0 +1,38 @@
+# engine/math/vector3.py
+
+import math
+
+class Vector3:
+    def __init__(self, x=0.0, y=0.0, z=0.0):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def __add__(self, other):
+        return Vector3(self.x + other.x, self.y + other.y, self.z + other.z)
+
+    def __sub__(self, other):
+        return Vector3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+    def __mul__(self, scalar):
+        return Vector3(self.x * scalar, self.y * scalar, self.z * scalar)
+
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+
+    def normalize(self):
+        mag = self.magnitude()
+        return Vector3(self.x / mag, self.y / mag, self.z / mag) if mag != 0 else Vector3(0, 0, 0)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y + self.z * other.z
+
+    def cross(self, other):
+        return Vector3(
+            self.y * other.z - self.z * other.y,
+            self.z * other.x - self.x * other.z,
+            self.x * other.y - self.y * other.x
+        )
+
+    def __repr__(self):
+        return f"Vector3({self.x}, {self.y}, {self.z})"
