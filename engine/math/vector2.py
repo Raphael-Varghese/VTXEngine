1c8b3e533204474c3cabe68edea04c7bad032346
Code_Boss <s20170468@students.duvalschools.org>
2025-10-07 10:45:37 -0400

    Create vector2.py

---
+++ engine/math/vector2.py
@@ -0,0 +1,30 @@
+# engine/math/vector2.py
+
+import math
+
+class Vector2:
+    def __init__(self, x=0.0, y=0.0):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        return Vector2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self, other):
+        return Vector2(self.x - other.x, self.y - other.y)
+
+    def __mul__(self, scalar):
+        return Vector2(self.x * scalar, self.y * scalar)
+
+    def magnitude(self):
+        return math.sqrt(self.x**2 + self.y**2)
+
+    def normalize(self):
+        mag = self.magnitude()
+        return Vector2(self.x / mag, self.y / mag) if mag != 0 else Vector2(0, 0)
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def __repr__(self):
+        return f"Vector2({self.x}, {self.y})"
